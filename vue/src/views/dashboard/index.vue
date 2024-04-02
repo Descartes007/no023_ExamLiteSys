9611285a0d87f6b94ad072b9cce5b4600e0b65b5
Descartes007 <zd_1025@163.com>
2024-04-02 20:25:50 +0800

    基于SSM的在线学习考试系统

---
+++ vue/src/views/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dashboard-container" style="text-align: center;">
+    <img src="@/assets/haoyun.jpg" style="width: 50%;height: 400px;margin:0 auto;">
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+
+  }
+}
+</script>
